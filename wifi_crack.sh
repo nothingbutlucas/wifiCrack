7a54fe275749d95cf4d0a289c6d04fef781638f6
lucas <lucasblancodev@gmail.com>
2022-10-15 18:07:37 -0300

    Posibility to not install the dependencies on the script

--- wifi_crack.sh
+++ wifi_crack.sh
@@ -97,22 +97,36 @@ function help_panel(){
 
 function check_installer_manager(){
     confirmation="y"
-    if [[ -f /usr/bin/apt ]]; then
-        installer="apt"
-    elif [[ -f /usr/bin/yum ]]; then
-        installer="yum"
-    elif [[ -f /usr/bin/pacman ]]; then
-        installer="pacman"
-        confirmation="Sy"
+    managers=(apt-get apt yum dnf zypper pacman emerge urpmi flatpak snap snapd pkg)
+    available_managers=()
+    for manager in "${managers[@]}"; do
+        if command -v $manager &> /dev/null; then
+            available_managers+=($manager)
+        fi
+    done
+    if [ "${#available_managers[@]}" -eq "0" ]; then
+        echo -e "\n${warn}[!]${nc} No package manager found"
     else
-        echo -e "${wrong}[!]${nc}No package manager found"
+        if [ "${#available_managers[@]}" -eq "1" ]; then
+            package_manager=${available_managers[0]}
+        else
+            echo -e "\n${warn}[!]${nc} More than one package manager found"
+            echo -e "\n${info}[·]${nc} Available package managers:"
+            PS3="[?] Select a package manager: "
+            select installer in "${available_managers[@]}"; do
+                if [ -n "$installer" ]; then
+                    package_manager=$installer
+                    break
+                fi
+            done
+        fi
     fi
 }
 
 function install_hcxdumptool(){
     echo -e "${doing}[~]${nc} Installing hcxdumptool..."
     sleep 0.5
-    if [[ $installer == "apt" ]]; then
+    if [[ $package_manager == "apt" ]]; then
         apt install -y libcurl4-openssl-dev libssl-dev pkg-config &>/dev/null
     fi
     git clone https://github.com/ZerBea/hcxdumptool.git &>/dev/null
@@ -135,11 +149,45 @@ function check_kaonashi(){
         mkdir -p /usr/share/wordlists/
         wget https://github.com/kaonashi-passwords/Kaonashi/blob/master/wordlists/kaonashiWPA100M.7z.torrent &>/dev/null
         sleep 0.5
-        exit_script
     else
         echo -e "${good}[+]${nc} kaonashi found"
         sleep 0.5
     fi
+}
+
+function install_all_missing_dependencies(){
+    $package_manager &> /dev/null
+    if [[ $program == "hcxdumptool" ]] && [[ $package_manager != "pacman" ]]; then
+        install_hcxdumptool
+    elif [[ $program == "hcxdumptool" ]] && [[ $package_manager == "pacman" ]]; then
+        $package_manager install -Sy $program &> /dev/null
+    fi
+    if [ "$(echo $?)" == "0" ]; then
+        echo -e "${doing}[~]${nc} Installing $program with $package_manager"
+        sleep 2
+        echo -e "${grey}$ sudo $package_manager install $program${nc}"
+        sudo $package_manager install $program -${confirmation} 1>/dev/null
+        if [ "$(echo $?)" == "0" ]; then
+            echo -e "${good}[+]${nc} $program has been installed"
+            installed_programs+=($program)
+        else
+            echo -e "${wrong}[-]${nc} $program could not be installed with $installer. Please, install it manually"
+        fi
+    else
+        echo -e "${wrong}[-]${nc} Could not install $program because I could not find a package manager on your system. Please, install it manually"
+    fi
+}
+
+function see_all_dependencies(){
+    echo -e "${info}[~]${nc} For handshakes attack:"
+    echo -e "\t${info}[~]${nc} aircrack-ng -> For listing the networks and capturing the handshake"
+    echo -e "\t${info}[~]${nc} tshark -> For reading the handshake file and verify it"
+    echo -e "${info}[~]${nc} For PMKID attack:"
+    echo -e "\t${info}[~]${nc} hcxdumptool -> For capturing the PMKID"
+    echo -e "\t${info}[~]${nc} hashcat -> For cracking the PMKID"
+    echo -e "${info}[~]${nc} For all the attacks"
+    echo -e "\t${info}[~]${nc} macchanger -> For changing your MAC address and annonymize yourself"
+    echo -e "\t${info}[~]${nc} kaonashi -> For cracking the handshakes and PMKID"
 }
 
 function dependencies(){
@@ -147,33 +195,15 @@ function dependencies(){
     programs=(aircrack-ng macchanger hcxdumptool hashcat tshark)
 
     echo -e "\n${doing}[~]${nc} Checking dependencies...\n"
-    sleep 2
+    sleep 0.5
 
     installed_programs=()
+    missing_dependencies=()
 
     for program in "${programs[@]}"; do
         if ! command -v $program &> /dev/null; then
-            check_installer_manager
             echo -e "${yellow}[*] ${nc}$program could not be found"
-            $installer &> /dev/null
-            if [[ $program == "hcxdumptool" ]] && [[ $installer != "pacman" ]]; then
-                install_hcxdumptool
-            elif [[ $program == "hcxdumptool" ]] && [[ $installer == "pacman" ]]; then
-                $installer install -Sy $program &> /dev/null
-            elif [ "$(echo $?)" == "0" ]; then
-                echo -e "${doing}[~]${nc} Installing $program with $installer"
-                sleep 2
-                echo -e "${grey}$ sudo $installer install $program${nc}"
-                sudo $installer install $program -${confirmation} 1>/dev/null
-                if [ "$(echo $?)" == "0" ]; then
-                    echo -e "${good}[+]${nc} $program has been installed"
-                    installed_programs+=($program)
-                else
-                    echo -e "${wrong}[-]${nc} $program could not be installed with $installer. Please, install it manually"
-                fi
-            else
-                echo -e "${wrong}[-]${nc} Could not install $program because I could not find a package manager on your system. Please, install it manually"
-            fi
+            missing_dependencies+=($program)
         else
             echo -e "${good}[+]${nc} $program found"
             sleep 0.5
@@ -182,21 +212,33 @@ function dependencies(){
     done
 
     check_kaonashi
+
+    if [ ${#missing_dependencies} -eq 0 ]; then
+        echo -e "${purple}[?]${nc} Do you want to install the missing dependencies? [y/n]"
+        echo -e "${blue}[·]${nc} My recommendation is to install them but by yourself, you never know who make the package you are installing..."
+        echo -e "${blue}[·]${nc} If you want to install them by yourself, just press n, install the dependencies and later run the script again (Make sure that the dependencies are somewhere on your path)"
+        echo -e "${blue}[·]${nc} If you are not so paranoid, just press y and let me install them for you"
+        read confirmation
+        if [[ ${confirmation,,} == "y" ]]; then
+            check_installer_manager
+            install_all_missing_dependencies
+        else
+            echo -e "${green}[+]${nc} Ok, I will not install the missing dependencies"
+            echo -e "${green}[+]${nc} Great for you! You are so paranoid that you do not trust me to install the missing dependencies for you, but no so paranoid to run this script with sudo xD"
+            sleep 0.5
+        fi
+    fi
     programs=(aircrack-ng macchanger hcxdumptool hashcat tshark)
     if [[ ${installed_programs} == ${programs} ]]; then
         echo -e "\n${good}[+]${nc} All dependencies are installed"
         sleep 1
     else
         echo -e "\n${wrong}[-]${nc} Some dependencies are not installed"
+        for dependency in "${missing_dependencies[@]}"; do
+            echo -e "${wrong}[-]${nc} $dependency is not installed"
+        done
         echo -e "${info}[~]${nc} Please, install them manually and run the script again"
-        echo -e "${info}[~]${nc} For handshakes attack:"
-        echo -e "\t${info}[~]${nc} macchanger -> For changing your MAC address and annonymize yourself"
-        echo -e "\t${info}[~]${nc} aircrack-ng -> For listing the networks and capturing the handshake"
-        echo -e "\t${info}[~]${nc} tshark -> For reading the handshake file and verify it"
-        echo -e "${info}[~]${nc} For PMKID attack:"
-        echo -e "\t${info}[~]${nc} macchanger -> For changing your MAC address and annonymize yourself"
-        echo -e "\t${info}[~]${nc} hcxdumptool -> For capturing the PMKID"
-        echo -e "\t${info}[~]${nc} hashcat -> For cracking the PMKID"
+        see_all_dependencies
         sleep 1
         exit_script
     fi
